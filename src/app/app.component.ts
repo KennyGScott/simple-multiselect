66cefb2cc3a268c7d5b1c55fbb70d84760a6c287
Kenny Scott <kscott@stevenswater.com>
2019-05-17 14:04:58 -0700

    basic filtering functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  public val;
+  public FilterOptions = [
+    {
+      filterTitle: 'Course 999',
+      course_id: 999
+    },
+    {
+      filterTitle: 'Course 117',
+      course_id: 117
+    }
+  ];
   public Course = {
     zones: [
       { id: 32, golf_course_id: 117, name: '01 Green' },
       { id: 79, golf_course_id: 117, name: '02 Green' },
       { id: 91, golf_course_id: 117, name: '03 Green' },
       { id: 49, golf_course_id: 117, name: '04 Green' },
-      { id: 70, golf_course_id: 117, name: '05 Green' },
+      { id: 70, golf_course_id: 999, name: '05 Green' },
       { id: 59, golf_course_id: 117, name: '06 Green' },
-      { id: 54, golf_course_id: 117, name: '07 Green' },
+      { id: 54, golf_course_id: 999, name: '07 Green' },
       { id: 18, golf_course_id: 117, name: '08 Green' },
       { id: 31, golf_course_id: 117, name: '09 Green' },
       { id: 79, golf_course_id: 117, name: '10 Green' },
-      { id: 86, golf_course_id: 117, name: '11 Green' },
+      { id: 86, golf_course_id: 999, name: '11 Green' },
       { id: 32, golf_course_id: 117, name: '12 Green' },
-      { id: 68, golf_course_id: 117, name: '13 Green' },
+      { id: 68, golf_course_id: 999, name: '13 Green' },
       { id: 63, golf_course_id: 117, name: '14 Green' },
       { id: 70, golf_course_id: 117, name: '15 Green' },
       { id: 99, golf_course_id: 117, name: '16 Green' },
@@ -29,8 +39,9 @@ export class AppComponent {
       { id: 52, golf_course_id: 117, name: '18 Green' }
     ]
   };
+
   log(event) {
-    console.log(event)
+    console.log(event);
   }
 }
 
--- src/app/simple-dropdown/simple-dropdown.component.ts
+++ src/app/simple-dropdown/simple-dropdown.component.ts
@@ -1,37 +1,66 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  // tslint:disable-next-line: component-selector
   selector: 'simple-dropdown',
   templateUrl: './simple-dropdown.component.html',
   styleUrls: ['./simple-dropdown.component.scss']
 })
 
-export class SimpleDropdownComponent {
+export class SimpleDropdownComponent implements OnInit {
   @Input() data: Array<object>;
-  @Input() filterOptions: Array<any>;
+  @Input() filterOptions: Array<object>;
   @Input() filterBy: string;
+  @Input() filterKey: string;
   @Input() titleKey: string;
+  @Input() type: string;
 
   @Output() selected = new EventEmitter();
 
-  private outputArray: Array<any>;
+  private outputArray: Array<object>;
+  public options: Array<object>;
+
   constructor() {
     this.outputArray = [];
+    this.type = (this.type) ? this.type : 'checkbox';
+  }
+
+  ngOnInit() {
+    this.initOptions();
+  }
+
+  initOptions() {
+    this.options = this.data;
+  }
+
+  filterEventHandler(event) {
+    this.initOptions();
+    const filterVal = event.target.value;
+    const filteredOptions = this.options.filter((option) => {
+      return option[this.filterBy] == filterVal;
+    });
+    this.options = filteredOptions;
+    this.returnData();
   }
 
   clickHandler(item, event) {
-    const checked = event.target.checked;
     const output = this.outputArray;
     const index = output.indexOf(item);
-    if (checked === true) {
-      output.push(item);
-    } else {
-      if (index !== -1) {
-        output.splice(index, 1);
+
+    if (this.type === 'checkbox') {
+      const checked = event.target.checked;
+      if (checked === true) {
+        output.push(item);
+      }
+      else {
+        if (index !== -1) {
+          output.splice(index, 1);
+        }
       }
     }
-    this.selected.emit(output);
+    this.options = output;
+    this.returnData();
   }
-
+  returnData() {
+    this.selected.emit(this.options);
+  }
 }
