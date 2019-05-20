5fa7781e909ac0d0a0a2025a1cabb11457bf308e
Kenny Scott <kscott@stevenswater.com>
2019-05-20 09:29:43 -0700

    remove bad filter setup

--- src/app/simple-dropdown/simple-dropdown.component.ts
+++ src/app/simple-dropdown/simple-dropdown.component.ts
@@ -7,60 +7,53 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 
 export class SimpleDropdownComponent implements OnInit {
-  @Input() data: Array<object>;
-  @Input() filterOptions: Array<object>;
-  @Input() filterBy: string;
-  @Input() filterKey: string;
-  @Input() titleKey: string;
-  @Input() type: string;
+  @Input() data: Array<object>
+  @Input() titleKey: string
+  @Input() type: string
 
-  @Output() selected = new EventEmitter();
+  @Output() selected = new EventEmitter()
 
-  private outputArray: Array<object>;
-  public options: Array<object>;
+  private selectedOptions: Array<object>
+  private outputArray: Array<object>
+  public isChecked = false
+  public options: Array<object>
 
   constructor() {
-    this.outputArray = [];
-    this.type = (this.type) ? this.type : 'checkbox';
+    this.type = (this.type) ? this.type : 'checkbox'
+    this.outputArray = []
   }
 
   ngOnInit() {
-    this.initOptions();
+    this.initOptions()
   }
 
   initOptions() {
-    this.options = this.data;
+    this.options = this.data.slice()
+
+    for (let i = this.options.length; i--;)
+      this.options[i]['selected'] = false
+
+    this.selectedOptions = []
   }
 
-  filterEventHandler(event) {
-    this.initOptions();
-    const filterVal = event.target.value;
-    const filteredOptions = this.options.filter((option) => {
-      return option[this.filterBy] == filterVal;
-    });
-    this.options = filteredOptions;
-    this.returnData();
+  checkHandler(item) {
+    const output = this.outputArray
+    const index = output.indexOf(item)
+    const selected = !item.selected
+
+    item.selected = selected
+
+    if (selected)
+      output.push(item)
+    else
+      if (index != -1)
+        output.splice(index, 1)
+
+    this.selectedOptions = output
+    this.returnData()
   }
 
-  clickHandler(item, event) {
-    const output = this.outputArray;
-    const index = output.indexOf(item);
-
-    if (this.type === 'checkbox') {
-      const checked = event.target.checked;
-      if (checked === true) {
-        output.push(item);
-      }
-      else {
-        if (index !== -1) {
-          output.splice(index, 1);
-        }
-      }
-    }
-    this.options = output;
-    this.returnData();
-  }
   returnData() {
-    this.selected.emit(this.options);
+    this.selected.emit(this.selectedOptions)
   }
 }
