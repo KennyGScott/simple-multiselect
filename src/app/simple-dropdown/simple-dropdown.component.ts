61f8c04245d2e2fcd8f2a468b964e8000295779b
Kenny Scott <kscott@stevenswater.com>
2019-05-20 10:38:31 -0700

    add comments, remove unnecessary code

--- src/app/simple-dropdown/simple-dropdown.component.ts
+++ src/app/simple-dropdown/simple-dropdown.component.ts
@@ -9,38 +9,51 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class SimpleDropdownComponent implements OnInit {
   @Input() data: Array<object>
   @Input() titleKey: string
-  @Input() type: string
   @Output() selected = new EventEmitter()
 
   public options;
   public checkAll = false;
 
-  constructor() {
-    this.type = (this.type) ? this.type : 'checkbox'
-  }
+  constructor() {}
 
   ngOnInit() {
     this.initOptions()
   }
 
+  /**
+   * @method initOptions handles initialization of dropdown options
+   * @param selected optional - manually overrides current value of item.selected
+   */
   initOptions(selected: boolean = false) {
+    // copy input data
     this.options = this.data.slice()
+    // add flag to each item for tracking selected objects
     for (let i = this.options.length; i--;)
       this.options[i]['selected'] = selected
   }
 
+  /**
+   * @method checkHandler handles the change event for an option's checkbox
+   * @param item current option being selected
+   */
   checkHandler(item) {
     const selected = !item.selected
     item.selected = selected
     this.returnData();
   }
 
+  /**
+   * @method handleAll handles the select all and deselct all
+   */
   handleAll() {
     this.checkAll = !this.checkAll
     this.initOptions(this.checkAll)
     this.returnData();
   }
 
+  /**
+   * @method returnData emits the selected event to pass the selected options to this components parent
+   */
   returnData() {
     const selectedOptions = this.options.filter((opt) => opt.selected == true)
     this.selected.emit(selectedOptions)
